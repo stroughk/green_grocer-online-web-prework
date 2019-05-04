0b9914a840fa92e339fd4e384c4bcab72bf861e7
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-04 22:52:20 +0000

    Automatically backed up by Learn

---
+++ random_grocery_generator.rb
@@ -0,0 +1,15 @@
+require_relative 'grocer'
+
+def items
+	[
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"KALE" => {:price => 3.00, :clearance => false}},
+		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+		{"ALMONDS" => {:price => 9.00, :clearance => false}},
+		{"TEMPEH" => {:price => 3.00, :clearance => true}},
+		{"CHEESE" => {:price => 6.50, :clearance => false}},
+		{"BEER" => {:price => 13.00, :clearance => false}},
+		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+		{"BEETS" => {:price => 2.50, :clearance => false}}
+	]
+end
